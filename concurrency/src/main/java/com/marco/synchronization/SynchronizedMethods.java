19c5d728dc7c003a48f890bdf201aea55a384707
iammarcode <26892819+iammarcode@users.noreply.github.com>
2024-07-04 00:52:17 +0800

    sync

---
+++ concurrency/src/main/java/com/marco/synchronization/SynchronizedMethods.java
@@ -0,0 +1,18 @@
+package com.marco.synchronization;
+
+public class SynchronizedMethods {
+    private int sum = 0;
+
+    public void calculate() {
+        setSum(getSum() + 1);
+        System.out.println(Thread.currentThread().getName() + " is running: " + this.sum);
+    }
+
+    public void setSum(int sum) {
+        this.sum = sum;
+    }
+
+    public int getSum() {
+        return this.sum;
+    }
+}
