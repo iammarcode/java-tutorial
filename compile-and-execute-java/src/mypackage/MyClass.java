c63fe8048d3e08c2ba6f26a6e6eb22d6631c7a9c
marco chow <26892819+iammarcode@users.noreply.github.com>
2024-12-05 15:23:33 +0800

    compile and execute java file

---
+++ compile-and-execute-java/src/mypackage/MyClass.java
@@ -0,0 +1,9 @@
+package mypackage;
+
+public class MyClass {
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println("H");
+    }
+}
